81ea7306e22553cdc501f33f66f7021849eed177
happy-join-github1 <happy-join-github1@noreply.gitcode.com>
2024-12-29 01:19:41 +0800

    Version 1.1

---
+++ js/NotFound-TnL3CjMd.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-1kcOvSsv.js";import{p as n,k as o}from"./vendor-0eqES6wC.js";const e=r({},[["render",function(r,e,s,t,a,d){return o(),n("h1",null,"此项目未开发，请等待开发")}]]);export{e as default};
---
+++ js/usebase-LXLjafin.js
@@ -0,0 +1,1 @@
+import{d as s}from"./index-1kcOvSsv.js";const t=s("AforB",{state:()=>({answer:[],result:[],list:[]}),actions:{sendmsg(s){this.result=s},solutionMsg(){return{userresult:this.result,okresult:this.answer,list:this.list}},init(s,t){this.answer=t,this.list=s}}});export{t as u};
